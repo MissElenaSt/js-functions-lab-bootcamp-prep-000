d1c486288f9faebc02d48ad36d25661d5a9d7892
Elena St <elenaperfectworld@aol.com>
2018-08-14 01:53:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  return 'Happy holidays, ${name}!'
+  return 'Happy holidays, ${name}!';
 }
 function happyHolidayTo(holiday, name){
-  return 'Happy ${holiday}, ${name}!'
+  return 'Happy ${holiday}, ${name}!';
 }
 function holidayCountdown(holiday, number){
-  return 'It\'s ${number} days until ${holiday}!'
+  return 'It\'s ${number} days until ${holiday}!';
 }
